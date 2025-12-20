14f89cf4a8552d623ca76e99b9c63b74802faf57
1nrez <formyyworkk@gmail.com>
2025-12-20 16:17:59 +0100

    finally(maybe)

--- main/config.py
+++ main/config.py
@@ -8,13 +8,13 @@ API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 
 DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "QWERTY!&UU")
-DB_NAME = os.getenv("DB_NAME", "TickRick")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_NAME = os.getenv("DB_NAME", "")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = int(os.getenv("DB_PORT", 5432))
 
-SOURCE_CHANNELS = ["TickRick_test", "sourcee_2"]
-TARGET_CHANNELS = ["@TickRick_target", "@targeet_2"]
+SOURCE_CHANNELS = [" ", " ", ] #без пробелов если нет в названии и тут без @
+TARGET_CHANNELS = ["@ ", "@ "] #оже самое только с @
 
-BOT_USER_ID = "@TickRick_bot"
+BOT_USER_ID = "@" #юзернейм бота
 ADMIN_USER_ID = int(os.getenv("ADMIN_USER_ID", "123456789"))
